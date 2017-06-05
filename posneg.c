a48d802fb61e92e2bab39d13b8134cf9732244fd
harinins <harinins09@gmail.com>
2017-06-05 12:10:12 +0530

    Add files via upload

---
+++ posneg.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,n;
+printf("\nenter n:");
+scanf("%d",&n);
+if(n==0)
+printf("\nnumber is zero");
+else if(n>=0)
+printf("\nnumber is positive");
+else
+printf("\nnumber is negative");
+}
