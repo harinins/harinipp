92acf7336177584b3a76fbde0293e1c8a566906d
harinins <harinins09@gmail.com>
2017-06-29 14:51:21 +0530

    Add files via upload

---
+++ Oddinterval.java
@@ -0,0 +1,21 @@
+package harini;
+
+import java.util.Scanner;
+
+public class Oddinterval {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,low,high;
+System.out.println("enter two values:");
+Scanner sc=new Scanner(System.in);
+low=sc.nextInt();
+high=sc.nextInt();
+for(i=low;i<=high;i++){
+	if(i%2!=0){
+		System.out.println(i);
+	}
+}
+	}
+
+}
