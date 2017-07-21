27d94b8ded8ea4326cda51a6c6173e88a8f1e881
harinins <harinins09@gmail.com>
2017-07-21 18:01:53 +0530

    Create Subset.java

---
+++ Subset.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.Scannner;
+class Oddeven{
+public static void main(String[] args){
+String s="harini";
+String s1="arhnii";
+boolean isSubset=s.containsAll(s1);
+System.out.println(isSubset);
+}
+}
