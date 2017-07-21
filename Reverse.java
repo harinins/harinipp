346367559ba801fe754dcd7821d84ec901aba9ea
harinins <harinins09@gmail.com>
2017-07-21 09:47:04 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,9 @@
+import java.io.*;
+impot java.util.Scanner;
+class Reverse{
+public static void main(String[]args){
+String s="";
+StringBuffer b=new StringBuffer(s).reverse;
+System.out.println(b);
+}
+}
