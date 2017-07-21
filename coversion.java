a5155bdf7e6d5cb7e28d031f2dba098507d4314f
harinins <harinins09@gmail.com>
2017-07-21 11:47:42 +0530

    Create coversion.java

---
+++ coversion.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.Scanner;
+class Conversion{
+public static void main(String[] args){
+String s="4500";
+int a=Integer.parseInt(s);
+System.out.println(a+1000);
+}
+}
